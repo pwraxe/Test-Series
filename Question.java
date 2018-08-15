d1e0cbd27bb0c02e59060d370d23def8c43f4c5f
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2018-08-15 16:57:20 +0530

    Add files via upload

---
+++ Question.java
@@ -0,0 +1,39 @@
+
+package Aptitude;
+
+public class Question 
+{
+    static char ChAns[] = {'A','D','B','A','D','D','C','A','D','D'};
+    //String ans[] = {"A. Largest Railway Station","D. All of the above","B. Nazi Party","A. Dr. G. D. Bist","D. Ultraviolet radiation","D. All of the above","C. Relation between organisms and their environment","A. 1958 in Tokyo","D. All of the above","D. Sri Lanka"};
+    static String question[] = {
+            "Q1. Grand Central Terminal, Park Avenue, New York is the world's",
+            "Q2. For which of the following disciplines is Nobel Prize awarded?",    
+            "Q3. Hitler party which came into power in 1933 is known as",
+            "Q4. Fastest shorthand writer was",
+            "Q5. The ozone layer restricts",
+            "Q6. Euclid was",
+            "Q7. Ecology deals with",
+            "Q8. Hockey was introduced in the Asian Games in",
+            "Q9. Gabriel Daniel Fahrenheit was",
+            "Q10.The ancient port-site of Godavaya is located in which country?"};
+    
+    static String option1[] = {"A. Largest Railway Station","B. Highest Railway Station","C. longest railway station","D. None of the above"};      //A
+    static String option2[] = {"A. Physics and Chemistry","B. Physiology or Medicine","C. Literature, Peace and Economics","D. All of the above"};  //D
+    static String option3[] = {"A. Labour Party","B. Nazi Party","C. Ku-Klux-Klan","D. Democratic Party"};   //B
+    static String option4[] = {"A. Dr. G. D. Bist","B. J.R.D. Tata","C. J.M. Tagore","D. Khudada Khan"};   //A
+    static String option5[] = {"A. Visible light","B. Infrared radiation","C. X-rays and gamma rays","D. Ultraviolet radiation"};  //D
+    static String option6[] = {"A. Greek mathematician","B. Contributor of deductive principles of basis of geometry","C. Propounded the geometrical theorems","D. All of the above"};  //D
+    static String option7[] = {"A. Birds","B. Cell formation","C. Relation between organisms and their environment","D. Tissues"};    //C
+    static String option8[] = {"A. 1958 in Tokyo","B. 1962 in Jakarta","C. 1966 in Bangkok","D. 1970 in Bangkok"};   //A
+    static String option9[] = {"A. a German Physicist","B. Developed the mercury thermometer in 1714","C. Devised temperature scale","D. All of the above"};   //D
+    static  String option10[] = {"A. Malaysia","B. Indonesia","C. Myanmar","D. Sri Lanka"};  //D
+
+    
+    
+    
+    
+    
+    
+      
+    
+}
